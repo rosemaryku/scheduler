b259545f1a3fd9013b09b381541962215159eabe
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-23 16:20:20 -0500

    create appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.scss";
+import Header from "./Header";
+
+function Appointment(props) {
+  return <article className="appointment"></article>;
+}
+
+export default Appointment;
--- stories/index.js
+++ stories/index.js
@@ -3,6 +3,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -125,3 +128,12 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
