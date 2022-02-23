e42473882c1d45f28c7fc4c41131a7a7376b2efb
Rosemary Ku <rosemary.ku@hotmail.com>
2022-02-23 16:40:16 -0500

    add status component

--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -153,4 +154,5 @@ storiesOf("Appointment", module)
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
     />
-  ));
+  ))
+  .add("Status", () => <Status message="Deleting" />);
